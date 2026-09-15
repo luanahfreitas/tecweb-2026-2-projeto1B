d8168aaf87fd3644254c4a965dbeb759be1aef0a
luanahf <luanahf@al.insper.edu.br>
2026-09-14 21:13:17 -0300

    Tarefa 3 - PostgreSQL

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tecweb-2026-2-projeto1B
+
+docker run --rm --name pg-docker \
+  -e POSTGRES_PASSWORD=escolhaumasenha \
+  -d \
+  -p 5432:5432 \
+  -v "$HOME/docker/volumes/postgres:/var/lib/postgresql/data" \
+  postgres
--- getit/settings.py
+++ getit/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "getit.wsgi.application"
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'getit',
+        'USER': 'getituser',
+        'PASSWORD': 'getitsenha',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
